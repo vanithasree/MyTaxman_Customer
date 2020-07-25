0d6c91353e01b6d1dca05b3a9ac75d6a7de72862
vanithasree <sreevanithaf12@gmail.com>
2020-07-25 12:11:07 +0530

    Added Tabbars

---
+++ MyTaxman_Customer/ViewControllers/Chat/ChatListViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ChatListViewController.swift
+//  MyTaxman_Customer
+//
+//  Created by Apple on 25/07/20.
+//  Copyright © 2020 Apple. All rights reserved.
+//
+
+import UIKit
+
+class ChatListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
